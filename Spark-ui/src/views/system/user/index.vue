0c352f9e25997abc22e14bdd057e6c843e9d77a5
DESKTOP-5T6BM88\Administrator <306702227@qq.com>
2021-04-08 20:57:53 +0800

    修改用户显示列

--- Spark-ui/src/views/system/user/index.vue
+++ Spark-ui/src/views/system/user/index.vue
@@ -8,12 +8,15 @@
     >
       <div slot="header" class="clearfix">
         <span>用户列表</span>
-        <el-button style="float: right; padding: 3px 0" type="text"
-          >新增用户</el-button
-        >
-        <el-button style="float: right; padding: 3px 0" type="text"
-          >导出用户</el-button
-        >
+        <el-button
+          style="float: right; padding: 3px 0"
+          type="text"
+        >新增用户</el-button>
+        <el-button
+          style="float: right; padding: 3px 0"
+          type="text"
+        >导出用户
+        </el-button>
       </div>
     </el-card>
 
@@ -25,35 +28,52 @@
         :header-cell-style="headerCellStyle"
         :cell-style="cellStyle"
       >
-        <el-table-column type="selection" width="55"> </el-table-column>
-        <el-table-column label="日期" width="180">
+        <el-table-column type="selection" width="55" />
+
+        <el-table-column label="#" type="index" width="50" />
+
+        <el-table-column label="账号" width="180">
           <template slot-scope="scope">
-            <i class="el-icon-time"></i>
-            <span style="margin-left: 10px">{{ scope.row.date }}</span>
+            <span>{{ scope.row.account }}</span>
           </template>
         </el-table-column>
-        <el-table-column label="姓名" width="180">
+        <el-table-column label="邮箱" width="180">
           <template slot-scope="scope">
-            <el-popover trigger="hover" placement="top">
-              <p>姓名: {{ scope.row.name }}</p>
-              <p>住址: {{ scope.row.address }}</p>
-              <div slot="reference" class="name-wrapper">
-                <el-tag size="medium">{{ scope.row.name }}</el-tag>
-              </div>
-            </el-popover>
+            <span size="medium">{{ scope.row.email }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column label="电话" width="180">
+          <template slot-scope="scope">
+            <span size="medium">{{ scope.row.phone }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column label="性别" width="180">
+          <template slot-scope="scope">
+            <span size="medium">{{ scope.row.sexName }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column label="生日" width="180" :formatter="dateFormat">
+          <template slot-scope="scope">
+            <span style="margin-left: 10px">{{ scope.row.birthday }}</span>
+          </template>
+        </el-table-column>
+
+        <el-table-column label="租户ID" width="180">
+          <template slot-scope="scope">
+            <el-tag size="medium">{{ scope.row.tenantId }}</el-tag>
           </template>
         </el-table-column>
         <el-table-column label="操作">
           <template slot-scope="scope">
-            <el-button size="mini" @click="handleEdit(scope.$index, scope.row)"
-              >编辑</el-button
-            >
+            <el-button
+              size="mini"
+              @click="handleEdit(scope.$index, scope.row)"
+            >编辑</el-button>
             <el-button
               size="mini"
               type="danger"
               @click="handleDelete(scope.$index, scope.row)"
-              >删除</el-button
-            >
+            >删除</el-button>
           </template>
         </el-table-column>
       </el-table>
@@ -62,105 +82,54 @@
 </template>
 
 <script>
-import { list as getUserList } from "@/api/user";
+import { list as getUserList } from '@/api/user'
 export default {
   data() {
     return {
-      tableData: [
-        {
-          date: "2016-05-02",
-          name: "王小虎x",
-          address: "上海市普陀区金沙江路 1518 弄",
-        },
-        {
-          date: "2016-05-02",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄",
-        },
-        {
-          date: "2016-05-02",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄",
-        },
-        {
-          date: "2016-05-02",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄",
-        },
-        {
-          date: "2016-05-02",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄",
-        },
-        {
-          date: "2016-05-02",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄",
-        },
-        {
-          date: "2016-05-02",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄",
-        },
-        {
-          date: "2016-05-04",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1517 弄",
-        },
-        {
-          date: "2016-05-01",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1519 弄",
-        },
-        {
-          date: "2016-05-03",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1516 弄",
-        },
-      ],
+      tableData: [],
       search: {
-        account: "",
+        account: '',
         current: 1,
         size: 10,
-        tenantId: "000000",
+        tenantId: '000000'
       }
-    };
+    }
   },
   created() {
-    this.init();
+    this.init()
   },
   methods: {
     handleEdit(index, row) {
-      console.log(index, row);
+      console.log(index, row)
     },
     handleDelete(index, row) {
-      console.log(index, row);
+      console.log(index, row)
     },
     init() {
-      this.list();
+      this.list()
     },
     list() {
-      //this.listLoading = true
-      getUserList(this.search).then((response) => {
-        this.data = response.data.records;
-        this.total = response.data.total;
+      // this.listLoading = true
+      getUserList(this.search).then(response => {
+        this.tableData = response.data.records
+        this.total = response.data.total
         //   this.listLoading = false
-      });
+      })
     },
     // 更改表头样式
     headerCellStyle({ row, column, rowIndex, columnIndex }) {
       if (rowIndex === 0) {
-        return "background-color: #fafafa;color: #000;font-weight: 500; text-align: center";
+        return 'background-color: #fafafa;color: #000;font-weight: 500; text-align: center'
       }
     },
     // 更改表头样式
     cellStyle({ row, column, rowIndex, columnIndex }) {
       // if (columnIndex !== 1 && columnIndex !== 3) {
-      return "text-align: center";
+      return 'text-align: center'
       // }
-    },
-  },
-};
+    }
+  }
+}
 </script>
 
 <style lang="scss" scoped>
